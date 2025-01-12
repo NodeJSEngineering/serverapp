652c32fae50cf3e6771f75966515790409c13e5d
Gupta, Sunny <sunnyvales789@gmail.com>
2025-01-13 00:03:03 +0530

    update

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, 
 const port = 1001; //Save the port number where your server ill be listening
 var path = require('path');
+
 app.get('/', (req, res) => {        
     res.sendFile('index.html', {root: __dirname});      
 });
 
-app.use(express.static(path.join(__dirname, "builds/stock")));
+app.use(express.static(path.join(__dirname, "dist/material")));
 
 app.get('/stock', (req, res) => {       
-    res.sendFile(path.join(__dirname, 'builds/stock', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist/material', 'index.html'));
 });
 
 
